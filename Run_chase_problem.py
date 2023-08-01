83099367ea6ab9de97de70c39bf31fa2de21b0a6
Tauntmoy <foysalkabir2017@gmail.com>
2023-08-02 01:36:42 +0600

    oop

--- Run_chase_problem.py
+++ Run_chase_problem.py
@@ -1,8 +1,11 @@
 import random
 
+
 def fitness(selected_batsmen, avg_runs, target_run):
-    total_runs = sum(avg_runs[i] for i in range(len(selected_batsmen)) if selected_batsmen[i])
+    total_runs = sum(avg_runs[i] for i in range(
+        len(selected_batsmen)) if selected_batsmen[i])
     return 1 if total_runs == target_run else 0
+
 
 def crossover(parent1, parent2):
     crossover_point = random.randint(1, len(parent1) - 1)
@@ -10,11 +13,14 @@ def crossover(parent1, parent2):
     child2 = parent2[:crossover_point] + parent1[crossover_point:]
     return child1, child2
 
+
 def mutate(selected_batsmen, mutation_rate):
     for i in range(len(selected_batsmen)):
         if random.random() < mutation_rate:
-            selected_batsmen[i] = 1 - selected_batsmen[i]  # Flip 0 to 1 or 1 to 0
+            # Flip 0 to 1 or 1 to 0
+            selected_batsmen[i] = 1 - selected_batsmen[i]
     return selected_batsmen
+
 
 def create_population(num_batsmen, population_size):
     population = []
@@ -23,10 +29,12 @@ def create_population(num_batsmen, population_size):
         population.append(selected_batsmen)
     return population
 
+
 def genetic_algorithm(num_batsmen, avg_runs, target_run, population_size, max_iterations, mutation_rate):
     population = create_population(num_batsmen, population_size)
     for generation in range(max_iterations):
-        fitness_scores = [fitness(selected_batsmen, avg_runs, target_run) for selected_batsmen in population]
+        fitness_scores = [fitness(selected_batsmen, avg_runs, target_run)
+                          for selected_batsmen in population]
 
         # Check if the target run is achieved
         if 1 in fitness_scores:
@@ -34,7 +42,12 @@ def genetic_algorithm(num_batsmen, avg_runs, target_run, population_size, max_it
             best_individual = population[best_individual_index]
             return best_individual
 
-        selected_parents = random.choices(population, weights=fitness_scores, k=population_size)
+        # Check if all fitness scores are 0, meaning no valid combination is found
+        if all(score == 0 for score in fitness_scores):
+            return None
+
+        selected_parents = random.choices(
+            population, weights=fitness_scores, k=population_size)
 
         next_generation = []
         for i in range(0, population_size, 2):
@@ -50,6 +63,7 @@ def genetic_algorithm(num_batsmen, avg_runs, target_run, population_size, max_it
     # If the target run is not achieved, return None
     return None
 
+
 def main():
     num_batsmen, target_run = map(int, input().split())
 
@@ -62,16 +76,19 @@ def main():
     max_iterations = 1000
     mutation_rate = 0.1
 
-    result = genetic_algorithm(num_batsmen, avg_runs, target_run, population_size, max_iterations, mutation_rate)
+    result = genetic_algorithm(
+        num_batsmen, avg_runs, target_run, population_size, max_iterations, mutation_rate)
 
     if result is None:
         print("No valid combination found.")
     else:
         print("Players and their selection:")
         for i in range(num_batsmen):
-            print(f"{avg_runs[i]} - {'Selected' if result[i] == 1 else 'Not Selected'}")
+            print(
+                f"{avg_runs[i]} - {'Selected' if result[i] == 1 else 'Not Selected'}")
 
         print("Selected batsman array:", "".join(str(x) for x in result))
+
 
 if __name__ == "__main__":
     main()
